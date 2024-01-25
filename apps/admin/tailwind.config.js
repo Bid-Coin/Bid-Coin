c049be32de05a0dd0a482ae7dcadd168d5d975af
hoinlee-moi <lhi3048@gmail.com>
2024-01-25 20:58:14 +0900

    test : monorepo ui test

--- apps/admin/tailwind.config.js
+++ apps/admin/tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 import config from '@pacakage/ui/tailwind.config.js';
-// const config = require('../../tailwind.config.cjs');
 
 const tlconfig = {
   ...config,
-  content: [
-    ...config.content,
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    '../../packages/ui/src/**/*.{ts,tsx,js,jsx}',
-  ],
+  content: [...config.content, './index.html', './src/**/*.{js,ts,jsx,tsx}'],
 };
 
 export default tlconfig;
--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,12 +1,12 @@
-import { Button, Heading } from '@pacakage/ui';
-
 export default function Home() {
   return (
-    <main>
-      <div className='w-96 h-96 bg-violet-500 flex justify-center items-center '>
-        <Heading title='드디어 성공함!!!' />
-      </div>
-      <Button className='p-10'>shadcn버튼성공</Button>
+    <main className='h-dvh w-screen bg-slate-100 flex flex-col items-center p-20'>
+      <section>
+        <h2>상품 정보랑 경매 참여</h2>
+      </section>
+      <section>
+        <h2>상품 이미지들</h2>
+      </section>
     </main>
   );
 }
